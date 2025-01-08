4097d30f0023a6c0d9f8b63aeda54063405722e8
Aditya Pandey <adityapandey.inbox@gmail.com>
2025-01-09 00:40:25 +0530

    all hindi articles stored in js array instead of html

--- articles.js
+++ articles.js
@@ -1,5 +1,221 @@
 const articles = [
     {
+        href: "https://hindi.mongabay.com/2021/06/25/niti-aayog-sdg-ranking-includes-climate-related-data-but-adds-to-confusion/",
+        target: "_blank",
+        topic: " ",
+        language: "Hindi",
+        publication: "Mongabay",
+        imgSrc: "Images/8320713187_b061fa00ba_k-2048x890.jpg",
+        imgAlt: "Image 1",
+        details: {
+            publicationName: "MONGABAY",
+            day: "25",
+            suffix: "",
+            month: "जून",
+            year: "2021"
+        },
+        title: "एसडीजी रैंकिंग में पर्यावरण से भी जुड़े मुद्दे पर नीति आयोग के तरीके ने बढ़ाई उलझन",
+        description: "कम से कम 12 राज्यों और केंद्र शासित प्रदेशों में इक्स्ट्रीम वेदर ईवेंट जैसे बाढ़, लू, बिजली गिरने इत्यादि से या तो कोई मौत हुई नहीं है या इन राज्यों में इन मौतों की गिनती के लिए कोई व्यवस्था नहीं है। हाल ही में आई नीति आयोग की रिपोर्ट से यही बात निकलती है जबकि केंद्र सरकार के अपने आंकड़े बताते हैं कि बिहार, गोवा या झारखंड में ऐसी कई मौतें हुई हैं। ये वही राज्य हैं जिन्होंने नीति आयोग के हालिया रिपोर्ट में मौसम की मार ..."
+    },
+    {
+        href: "https://hindi.mongabay.com/2020/12/02/control-over-poaching-led-to-increase-tiger-population-in-pilibhit/",
+        target: "_blank",
+        topic: " ",
+        language: "Hindi",
+        publication: "Mongabay",
+        imgSrc: "Images/61a7526a-4bbc-4fc3-81d8-d37c2414bb4e-1.jpg",
+        imgAlt: "Image 1",
+        details: {
+            publicationName: "MONGABAY",
+            day: "2",
+            suffix: "",
+            month: "दिसम्बर",
+            year: "2020"
+        },
+        title: "उत्तरप्रदेश के पीलीभीत में जब खत्म हुए शिकारी तो तिगुना हुआ बाघ का कुनबा",
+        description: "माना जाता है कि बाघ की दहाड़ करीब तीन किलोमीटर तक सुनी जा सकती है। विडंबना यह है कि अपनी बहादुरी के लिए चर्चित यह सुंदर जीव भी खतरे में है। पूरे विश्व में बाघों की संख्या घटकर कुछेक हजार पर सिमट गई है। ऐसी स्थिति कैसे आई और कैसे पुरानी स्थिति बहाल हो- इस वैश्विक समस्या और उसके समाधान का एक छोटा सा नमूना पेश करता है पीलीभीत का टाइगर रिजर्व ..."
+    },
+    {
+        href: "https://hindi.mongabay.com/2020/12/22/pradhan-mantri-ujjwala-yojana-fails-to-introduce-clean-fuel-in-kitchen/",
+        target: "_blank",
+        topic: " ",
+        language: "Hindi",
+        publication: "Mongabay",
+        imgSrc: "Images/IMG_20190322_174248-scaled.jpg",
+        imgAlt: "Image 1",
+        details: {
+            publicationName: "Mongabay",
+            day: "22",
+            suffix: "",
+            month: "दिसम्बर",
+            year: "2020"
+        },
+        title: "सरकारी सर्वेक्षण से खुली प्रधानमंत्री उज्जवला योजना की पोल, बहुत कम इस्तेमाल हो रहा एलपीजी",
+        description: "देश के प्रधानमंत्री नरेंद्र मोदी की कुछ बड़ी उपलब्धियों में ‘प्रधानमंत्री उज्जवला योजना’ की गिनती होती है। सरकार दावा करती है कि इस कार्यक्रम की मदद से देश के 98 फीसदी घरों में एलपीजी सिलेंडर उपलब्ध कराया जा चुका है। इस योजना को स्त्री सशक्तिकरण के मद्देनजर चलाए जा रहे सामाजिक आंदोलन के तौर पर भी ..."
+    },
+    {
+        href: "https://hindi.mongabay.com/2024/03/27/crafting-a-sustainable-future-for-artisans-using-bamboo/",
+        target: "_blank",
+        topic: " ",
+        language: "Hindi",
+        publication: "Mongabay",
+        imgSrc: "Images/Banner-image.jpg",
+        imgAlt: "Image 1",
+        details: {
+            publicationName: "Mongabay",
+            day: "27",
+            suffix: "",
+            month: "मार्च",
+            year: "2024"
+        },
+        title: "बांस के जरिए कारीगरों के लिए बेहतर भविष्य बनाने की कोशिश",
+        description: "ज्योत्सना देवनाथ का छोटी उम्र में पश्चिम त्रिपुरा के ब्रज नगर में नया सफर शुरू हुआ। यहां उनकी शादी हुई और वह एक ऐसे परिवार का हिस्सा बन गईं, जो बांस के उत्पाद बनाकर अपनी आजीविका चलाने के लिए जाना जाता था। उनके पति राजकुमार देवनाथ ने उन्हें कारोबार के कौशल में प्रशिक्षित किया। यानी बांस खरीदने से लेकर उत्पाद बनाने और इन्हें स्थानीय बाजार में बेचने तक। ..."
+    },
+    {
+        href: "https://hindi.mongabay.com/2021/01/11/part-of-heritage-gangetic-dolphin-facing-existential-threat/",
+        target: "_blank",
+        topic: " ",
+        language: "Hindi",
+        publication: "Mongabay",
+        imgSrc: "Images/A-surfacing-Gangetic-river-dolphin.-Photo-by-Ravindra-Sinha..jpg",
+        imgAlt: "Image 1",
+        details: {
+            publicationName: "Mongabay",
+            day: "11",
+            suffix: "",
+            month: "जनवरी",
+            year: "2021"
+        },
+        title: "जिस गंगा डॉल्फिन का जिक्र पौराणिक कथाओं में उसे क्यों भूलने लगे हम",
+        description: "पिछले सप्ताह एक बेचैन करने वाला विडियो वायरल हुआ जिसमें कुछ लोग एक डॉल्फिन पर लाठी, डंडे और कुल्हाड़ी जैसे हथियार से अंधाधुंध हमला करते दिखे। उसपर हमला करने वाले ये लोग उत्तर प्रदेश के प्रतापगढ़ जिले से हैं। शारदा नदी से जुड़े नहर में दिसंबर में हुए इस घटना में स्थानीय पुलिस ने अब तक तीन लोगों को गिरफ्तार कर लिया है और अन्य लोगों की तलाश जारी ..."
+    },
+    {
+        href: "https://hindi.mongabay.com/2021/03/04/pesa-fails-to-mark-its-existence-even-in-25-year-since-it-came-in-existence/",
+        target: "_blank",
+        topic: " ",
+        language: "Hindi",
+        publication: "Mongabay",
+        imgSrc: "Images/53083477_2130172720352692_6037181712599875584_n-1.jpg",
+        imgAlt: "Image 1",
+        details: {
+            publicationName: "Mongabay",
+            day: "4",
+            suffix: "",
+            month: "मार्च",
+            year: "2021"
+        },
+        title: "[वीडियो] पच्चीसवें साल में पेसा: ग्राम सभा को सशक्त करने के लिए आया कानून खुद कितना मजबूत!",
+        description: "पेसा यानी पंचायत (अनुसूचित क्षेत्रों में विस्तार) क़ानून को आए पच्चीस साल पूरे होने वाले हैं। आदिवासी बहुल इलाकों में स्थानीय समाज को मजबूती देने के लिए लाया गया यह कानून आज खुद की प्रासंगिकता के सवालों से जूझ रहा है। ..."
+    },
+    {
+        href: "https://hindi.mongabay.com/2021/01/28/an-idea-mooted-to-empower-farmers-by-building-grams-remains-on-paper/",
+        target: "_blank",
+        topic: " ",
+        language: "Hindi",
+        publication: "Mongabay",
+        imgSrc: "Images/A-woman-selling-farm-produce-in-local-haat-in-Jhabua-district-1200x800-1.jpg",
+        imgAlt: "Image 1",
+        details: {
+            publicationName: "Mongabay",
+            day: "28",
+            suffix: "",
+            month: "जनवरी",
+            year: "2021"
+        },
+        title: "ग्राम: छोटे किसानों को सही दाम दिलाने के लिए तीन साल पहले लायी गयी योजना कागजों तक सीमित",
+        description: "करीब दो महीने तक राजधानी की सीमा के बाहर हाड़ कंपा देने वाले सर्दी में संघर्ष करने के बाद लाखों किसानों गणतंत्र दिवस के दिन दिल्ली की सीमा में ट्रैक्टर जुलूस के साथ प्रवेश कर पाए। केंद्र सरकार द्वारा लाए गए  तीन कृषि कानूनों  का विरोध का विरोध कर रहे किसानों में से एक की मौत हो गयी और पुलिस के लाठी चार्ज में ..."
+    },
+    {
+        href: "https://hindi.mongabay.com/2021/03/31/using-fossil-fuel-as-a-cash-cow-a-big-obstacle-in-indias-energy-transition/",
+        target: "_blank",
+        topic: " ",
+        language: "Hindi",
+        publication: "Mongabay",
+        imgSrc: "Images/26739688217_2e98e7b378_o-scaled-e1617181245819.jpg",
+        imgAlt: "Image 1",
+        details: {
+            publicationName: "Mongabay",
+            day: "31",
+            suffix: "",
+            month: "मार्च",
+            year: "2021"
+        },
+        title: "पेट्रोल-डीजल से होती है सरकार को मोटी कमाई, फिर कैसे होगा एनर्जी ट्रांजिशन!",
+        description: "हाल में हुए पेट्रोल-डीजल के दामों में बेतहाशा वृद्धि ने कई तरह के सवाल खड़े किए। तब भी पेट्रोल-डीजल के दाम बढ़े रहे जब अंतर्राष्ट्रीय बाजार में कच्चे तेल की कीमत रही। ऐसा इसलिए हुआ क्योंकि सरकार ने इसपर करीब 60% का कर लगा रखा है। इसकी वजह से बढ़ती महंगाई और पेट्रोल डीजल की कीमत के सैकड़ा छूने की तो काफी चर्चा हो रही है। लेकिन एक बात पर चर्चा नहीं हो पा रही है ..."
+    },
+    {
+        href: "https://hindi.mongabay.com/2021/05/11/where-are-we-in-net-zero-debate/",
+        target: "_blank",
+        topic: " ",
+        language: "Hindi",
+        publication: "Mongabay",
+        imgSrc: "Images/49012565232_56dcf074e5_c.jpg",
+        imgAlt: "Image 1",
+        details: {
+            publicationName: "Mongabay",
+            day: "11",
+            suffix: "",
+            month: "मई",
+            year: "2021"
+        },
+        title: "बहस नेट जीरो की: कार्बन उत्सर्जन की लड़ाई में भारत के राज्य कहां खड़े हैं!",
+        description: "संयुक्त राज्य अमेरिका के नव निर्वाचित राष्ट्रपति जो बाइडेन के आह्वान पर, अप्रैल में लगभग 40 देशों ने नेट-जीरो हासिल करने के लिए समय-सीमा की घोषणा की। भारत के प्रधानमंत्री नरेंद्र मोदी ने ऐसी कोई घोषणा तो नहीं की लेकिन उसी वर्चुअल मुलाकात में 2030 तक अक्षय क्षमता के 450 गीगावाट स्थापित करने की अपनी प्रतिबद्धता दोहराई। इन सबको देखते हुए भारत ..."
+    },
+    {
+        href: "https://www.downtoearth.org.in/hindistory/health/communicable-disease/sufferers-then-sufferers-now-70318",
+        target: "_blank",
+        topic: " ",
+        language: "Hindi",
+        publication: "Down To Earth",
+        imgSrc: "Images/18.avif",
+        imgAlt: "Image 1",
+        details: {
+            publicationName: "Down To Earth",
+            day: "8",
+            suffix: "",
+            month: "अप्रैल",
+            year: "2020"
+        },
+        title: "महामारी से तब भी पीड़ित थे, अब भी पीड़ित हैं : कांचा इलैया",
+        description: "जाने-माने लेखक, राजनीतिक विश्लेषक और कार्यकर्ता कांचा इलैया शेपहर्ड के दादा-दादी 1897 के बुबोनिक प्लेग के शिकार हुए थे। इस महामारी ने अविभाजित भारत की करीब 6 प्रतिशत आबादी को लील लिया था। कांचा को इससे जुड़ी कहानियां विरासत में मिलीं। कुंदन पांडे के साथ बातचीत में वह उनके संघर्षों और जीवित रहने की रणनीतियों को याद कर रहे हैं। ये संघर्ष और ..."
+    },
+    {
+        href: "https://www.downtoearth.org.in/hindistory/health/women-health/doctors-mislead-women-to-get-their-uterus-removed-69745",
+        target: "_blank",
+        topic: " ",
+        language: "Hindi",
+        publication: "Down To Earth",
+        imgSrc: "Images/0.69458100_1584007502_carousel-(39).jpg",
+        imgAlt: "Image 1",
+        details: {
+            publicationName: "Down To Earth",
+            day: "8",
+            suffix: "",
+            month: "मार्च",
+            year: "2020"
+        },
+        title: "कोख की चोरी: डॉक्टरों ने हजारों महिलाओं को भ्रम में रखकर गर्भाशय निकालवाए",
+        description: "जनवरी में गन्ने की कटाई के समय भरी दोपहरी में गन्ना खेत मजदूर वंदना खंडाले अपने घर पर खाली बैठी हुई है। पूछने पर कहती है कि उसके घुटनों में भयानक दर्द हो रहा है और हमेशा थकान रहती है। खेत में काम करते हुए थक जाती है और इतनी झुंझलाहट होती है कि काम करना मुश्किल हो गया है। ..."
+    },
+    {
+        href: "https://hindi.downtoearth.org.in/wildlife-biodiversity/tribals/%E0%A4%AA%E0%A4%A5-%E0%A4%AA%E0%A5%8D%E0%A4%B0%E0%A4%A6%E0%A4%B0%E0%A5%8D%E0%A4%B6%E0%A4%BF%E0%A4%A4-63615",
+        target: "_blank",
+        topic: " ",
+        language: "Hindi",
+        publication: "Down To Earth",
+        imgSrc: "Images/0.61456800_1548742596_17-20190215-english.jpg",
+        imgAlt: "Image 1",
+        details: {
+            publicationName: "Down To Earth",
+            day: "18",
+            suffix: "",
+            month: "अप्रैल",
+            year: "2019"
+        },
+        title: "सामुदायिक भूमि पर अधिकार के लिए दलितों का आंदोलन",
+        description: "जनवरी की हाड़ कंपाती सर्दी और पंजाब की सुबह। करीब 5 बजे का वक्त। संगरूर जिले के बालदकलां गांव में ज्यादातर लोग नींद की गिरफ्त में थे पर बंत कौर उठ चुकी थीं। पचास की उम्र पार कर चुकीं विधवा बंत कौर हड़बड़ी में इधर-उधर दौड़कर रोजमर्रा के कामों को तेजी से निपटा रही थीं। उन्हें आठ बजे तक भैंस को खिला-पिलाकर दुहना था, खुद और पंद्रह साल के बेटे के लिए ..."
+    },
+    {
         href: "https://www.downtoearth.org.in/hindistory/governance//%E0%A4%86%E0%A4%9C%E0%A4%BE%E0%A4%A6%E0%A5%80-%E0%A4%95%E0%A4%BE-%E0%A4%B8%E0%A4%82%E0%A4%98%E0%A4%B0%E0%A5%8D%E0%A4%B7-60131",
         target: "_blank",
         topic: " ",
